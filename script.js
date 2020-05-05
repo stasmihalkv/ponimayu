7a5e8061eb150429792962b55aa31ed9eb24acb8
Stanislav <39649166+stasmihalkv@users.noreply.github.com>
2020-05-05 17:11:23 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-
-$(".tab_item").not(":first").hide();
-$(".block2_menu li").click(function() {
-	$(".block2_menu li").removeClass("active").eq($(this).index()).addClass("active");
-	$(".tab_item").hide().eq($(this).index()).fadeIn()
-}).eq(0).addClass("active");
-
-
-
+window.onload = function(){
+    
+    $(".tab_item").not(":first").hide();
+    $(".block2_menu li").click(function() {
+        $(".block2_menu li").removeClass("active").eq($(this).index()).addClass("active");
+        $(".tab_item").hide().eq($(this).index()).fadeIn()
+    }).eq(0).addClass("active");
 
     $(function(){
         $('.menu_toggle').on('click',function(){
@@ -17,4 +15,7 @@ $(".block2_menu li").click(function() {
             });
         });
     });
+
+}
+
 
